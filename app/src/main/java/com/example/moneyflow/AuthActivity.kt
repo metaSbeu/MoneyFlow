fd81b4d8f89db908805bbdaa8941ecc1f5d3c578
Vlad <suedis.suedis@gmail.com>
2025-02-25 14:54:49 +0500

    TransactionAdd screen done almost

--- app/src/main/java/com/example/moneyflow/AuthActivity.kt
+++ app/src/main/java/com/example/moneyflow/AuthActivity.kt
@@ -1,6 +1,8 @@
 package com.example.moneyflow
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,14 @@ class AuthActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        // Найдём кнопку "0" по её ID и установим обработчик нажатия
+        val buttonZero = findViewById<Button>(R.id.buttonZero)
+        buttonZero.setOnClickListener {
+            // Запускаем MainActivity
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
         }
     }
 }
