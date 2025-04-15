5129429a83b3979351b6a08bb8fdc9da790ad679
Sbeu <suedis.suedis@gmail.com>
2025-04-15 21:30:12 +0500

    -Auth activity logic completed -Added some card views on activity_main.xml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,16 +33,28 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
+
+    implementation ("io.github.chaosleung:pinview:1.4.4")
+    implementation ("androidx.biometric:biometric-ktx:1.4.0-alpha02")
+
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.lifecycle.livedata.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
 }
---
+++ app/src/main/java/com/example/moneyflow/AuthViewModel.kt
@@ -0,0 +1,10 @@
+package com.example.moneyflow
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class AuthViewModel(application: Application) : AndroidViewModel(application) {
+
+
+
+}
